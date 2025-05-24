088d0782c619d65314ab85ba82f68432c13a17b0
Marcos <mxndeveloper@gmail.com>
2025-05-24 17:12:56 +0300

    Final commit - Test files and contract implementations done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,6 +12,34 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // Code goes here
+  // Setup accounts
+  [deployer] = await ethers.getSigners()
+
+  // Deploy Dappazon Contract
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
+  const dappazon = await Dappazon.deploy()
+  await dappazon.deployed()
+
+  console.log(`Deployed Dappazon Contract at: ${dappazon.address}\n`)
+
+  // List items
+  for(let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+  }
+
+  await transaction.wait()
+
+  console.log(`Listed Item ${items[i].id}: ${items[i].name}`)
+
 
 }
 
